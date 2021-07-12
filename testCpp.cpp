74d8c52d34f19e605128af1144c0ecfd956a4f23
leooriginal <leooriginal@mail.ru>
2021-07-12 13:08:53 +0300

    testCpp.cpp modified

--- testCpp.cpp
+++ testCpp.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main() {
     int x,y;
-  cin >> x >> y;
-  cout << x/y;
+    double q,w;
+
+x = 6;
+y = 5;
+q = 3.14;
+w = 6.36;
+
+  cin >> q >> w;
+  cout << static_cast<int> (q+w) << endl;
+       //<< q*w << endl;
+
+  //cout << (x/y)*1.0 << endl
+  //     << static_cast<int>(q*w) << endl;
 }
